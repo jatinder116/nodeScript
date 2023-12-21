796dd78ca32eb194b838c16a2f72ae7e2cdcfbd7
Jatinder Singh <jatindersngh116@gmail.com>
2023-12-21 14:38:00 +0530

    check binary

--- validator_script.sh
+++ validator_script.sh
@@ -6,4 +6,4 @@ ADDRESS=$(shidod keys list --home $HOMEDIR --keyring-backend $KEYRING | grep "ad
 echo $ADDRESS
 
 #========== for creating validator ==========================================================================
-shidod tx staking create-validator --amount=100000000000000000000shido   --pubkey=$(./shidod tendermint show-validator --home $HOMEDIR)   --moniker="validator"   --chain-id=shido_9007-1   --commission-rate="0.10"   --commission-max-rate="0.20"   --commission-max-change-rate="0.01"   --min-self-delegation="10"   --gas="auto"   --gas-prices="0.025shido"   --from=$ADDRESS  --gas="auto" --gas-adjustment="1.15" --home $HOMEDIR --keyring-backend $KEYRING
+shidod tx staking create-validator --amount=100000000000000000000shido   --pubkey=$(shidod tendermint show-validator --home $HOMEDIR)   --moniker="validator"   --chain-id=shido_9007-1   --commission-rate="0.10"   --commission-max-rate="0.20"   --commission-max-change-rate="0.01"   --min-self-delegation="10"   --gas="auto"   --gas-prices="0.025shido"   --from=$ADDRESS  --gas="auto" --gas-adjustment="1.15" --home $HOMEDIR --keyring-backend $KEYRING
